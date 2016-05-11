2f17d4df2c41973cda19e8c194db47e99e573d39
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2016-05-11 23:49:34 +0100

    feat(detailed-service): add detailed service view

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -14,51 +14,51 @@ const ListItemCore = React.createClass({
         var mySVGsToInject = this.refs.root.querySelectorAll('img.inject-me');
         // Do the injection
         SVGInjector(mySVGsToInject);
+
+        $(this.refs.root)
+            .accordion();
+    },
+    handleAccordionClick: function () {
+        $(this.refs.root)
+            .accordion('toggle', 1);
     },
     render: function () {
         return (
-            <div className="ui segments" ref="root">
-                <div className="ui segment listitem active">
-                    <div className="ui grid middle aligned content">
-                        <div className="twelve wide tablet five wide computer column title good">
+            <div className="ui segments fluid accordion" ref="root">
+                <div className="ui segment listitem running">
+                    <div className="ui grid middle aligned">
+                        <div className="ui twelve wide tablet nine wide computer column title good" onClick={ this.props.onClick }>
                             <img className="ui right spaced image inject-me" src="assets/active_service_icon.svg"/>
                             <span>{ this.props.title }</span>
-                        </div>
-                        <div className="computer only four wide computer column version">
-                            Version 1.0
                         </div>
                         <div className="three wide computer only column">
                         </div>
                         <div className="four wide tablet four wide computer right aligned column">
-                            <div className="inline-button lightable">
+                            <div onClick={ this.handleAccordionClick }  className="inline-button lightable" >
                                 <img className="vertical-center inject-me" src="assets/more.svg" alt/>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="ui segment detailitem">
+                <div className="ui title"></div>
+                <div className="ui segment detailitem content">
+                    <div className="ui fluid">
                     <div className="ui grid">
                         <div className="two wide column border-right">
                         </div>
                         <div className="fourteen wide column">
                             <div className="ui one column grid">
                                 <div className="column">
-                                    <div className="ui grid middle aligned content">
+                                    <div className="ui grid middle aligned">
                                         <div className="sixteen wide column">
-                                            <i className="circle tiny icon red"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="column">
-                                    <div className="ui grid middle aligned content">
-                                        <div className="sixteen wide column">
-                                            <i className="circle icon red tiny"></i>
+                                           Version 1.0 <i className="circle tiny icon green"></i>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
                 </div>
             </div>
 
